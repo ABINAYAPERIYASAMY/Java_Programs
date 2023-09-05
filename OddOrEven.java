fcdbd9b1032bc179a255c484930d5e96a174d399
ABINAYAPERIYASAMY <abinaya26902@gmail.com>
2023-09-05 21:58:11 +0530

    Java Programs

---
+++ OddOrEven.java
@@ -0,0 +1,17 @@
+//package Abinaya;
+import java.util.*;
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          Scanner sc=new Scanner(System.in);
+          int n=sc.nextInt();
+          if(n%2==0)
+          {
+        	  System.out.println("Even");
+          }
+          else
+        	  System.out.println("Odd");
+	}
+
+}
