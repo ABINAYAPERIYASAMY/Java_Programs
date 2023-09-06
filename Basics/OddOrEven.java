ccfdaf6e782774c4d216f66f85080aab08f3ace4
ABINAYAPERIYASAMY <abinaya26902@gmail.com>
2023-09-06 09:36:47 +0530

    Initial commit

--- Basics/OddOrEven.java
+++ Basics/OddOrEven.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class OddOrEven {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
           Scanner sc=new Scanner(System.in);
           int n=sc.nextInt();
           if(n%2==0)
