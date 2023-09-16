456fcdac2f9e13820f9792dea00d20d5f0783cf2
ABINAYAPERIYASAMY <abinaya26902@gmail.com>
2023-09-16 18:43:24 +0530

    Element in Even Position

---
+++ ArrayBasicPrograms/ElementinEvenPosition.java
@@ -0,0 +1,18 @@
+package Abinaya;
+import java.util.*;
+public class ElementinEvenPosition {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();  
+		int[] array = new int[n];  
+		System.out.println("Enter the elements of the array: ");  
+		for(int i=0; i<n; i++)  
+		{    
+		array[i]=sc.nextInt();  
+		}  
+		System.out.println("Elements of given array present on odd position: ");  
+	    for (int i = 1; i < array.length; i = i+2) {  
+	            System.out.print(array[i]+" "); 
+	    }
+	}
+}
