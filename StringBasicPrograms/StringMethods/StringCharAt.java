3926cbeb8c5a5c10066153cfbe501fb714a223fa
ABINAYAPERIYASAMY <abinaya26902@gmail.com>
2023-09-19 15:09:35 +0530

    CharAt Method

---
+++ StringBasicPrograms/StringMethods/StringCharAt.java
@@ -0,0 +1,10 @@
+package Abinaya;
+public class StringCharAt {
+	public static void main(String[] args) {
+		 String str = "Welcome to Javatpoint portal";      
+		 int strLength = str.length();      
+		 System.out.println("Character at 0 index is: "+ str.charAt(0));      
+		 System.out.println("Character at 0 index is: "+ str.charAt(4));    
+		 System.out.println("Character at last index is: "+ str.charAt(strLength-1));
+	}
+}
